d42b3921dcb64b203c9b96e3bab82fd76899715a
choisungwook <kgg1959@naver.com>
2022-10-10 11:21:49 +0900

    MarkDown 파싱 모듈 추가

---
+++ app/lib/posts.ts
@@ -0,0 +1,23 @@
+import path from "path";
+import fs from "fs";
+import matter from "gray-matter";
+
+
+const postDirectory = path.join(process.cwd(), 'posts');
+
+// Markdown 데이터 로드 후, 파싱
+export function getSortedPostDataFromMarkDown() {
+  const fileNames: string[] = fs.readdirSync(postDirectory);
+
+  const allPostData = fileNames.map(fileName => {
+    const fileId = fileName.replace(/\.md$/, "")
+    const filePath = path.join(postDirectory, fileName);
+    const Contents = fs.readFileSync(filePath, 'utf-8');
+    const matterResult = matter(Contents);
+
+    return {
+      fileId,
+      ...matterResult.data as { data: string; title: string}
+    }
+  });
+}
